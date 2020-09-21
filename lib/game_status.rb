0a56dd4be5444006a0c67b306e6db9df13f74fb4
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-21 17:56:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,18 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def won
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [3,4,5],
+  [2,4,6],
+  [6,7,8],
+  [2,5,8]
+]
+
+def won?(board)
 end
 
 def full?(board)
@@ -14,6 +25,10 @@ def draw?(board)
   if full?(board) && !won?(board)
     true
   end
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
 end
 
 # Define your WIN_COMBINATIONS constant
