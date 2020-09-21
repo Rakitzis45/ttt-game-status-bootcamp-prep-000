ee30c79c53fadeecde41d3c16a92ae05d946c3d2
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-21 18:29:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,23 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |winner|
+  WIN_COMBINATIONS.each do |winner|
+    index_0 = winner[0]
+    index_1 = winner[1]
+    index_2 = winner[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      winner
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      winner
+    else
+      false
+    end
+  end
+    postion_taken?(board, winner[0])
 end
 
 def full?(board)
